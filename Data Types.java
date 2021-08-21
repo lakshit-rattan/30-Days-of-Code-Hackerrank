00e3b45dca3d5287d4e81b0b41a6240293d17a3e
Lakshit Rattan <68140682+lakshit-rattan@users.noreply.github.com>
2021-08-21 16:01:33 +0530

    Que-2

---
+++ Data Types.java
@@ -0,0 +1,22 @@
+
+
+        /* Declare second integer, double, and String variables. */
+        int a;
+        double b;
+        String c;
+        /* Read and save an integer, double, and String to your variables.*/
+     
+        a = scan.nextInt();
+        b = scan.nextDouble();
+        scan.nextLine();
+        c = scan.nextLine();
+
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println((i+a));
+        System.out.println((d+b));
+        System.out.println(s+c);
+        /* Print the sum of the double variables on a new line. */
+		
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+
