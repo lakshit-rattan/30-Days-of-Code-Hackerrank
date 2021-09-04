6e8b794ae6a52305361a82d98e67607baef9dcce
Lakshit Rattan <68140682+lakshit-rattan@users.noreply.github.com>
2021-09-04 11:37:17 +0530

    Scope

---
+++ Day-15.py
@@ -0,0 +1,9 @@
+
+
+    # Add your code here
+    def computeDifference(self):
+        maxn = max(self.__elements)
+        minn = min(self.__elements)
+        
+        self.maximumDifference = maxn-minn
+
