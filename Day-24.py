d7bfea573d146273f35efb5568337a172c8d84ab
Lakshit Rattan <68140682+lakshit-rattan@users.noreply.github.com>
2021-09-13 10:30:34 +0530

    BST Level-Order Traversal

---
+++ Day-24.py
@@ -0,0 +1,7 @@
+    def levelOrder(self,root):
+      queue = [root] if root else []
+      while queue:
+        node = queue.pop()
+        print(node.data, end=" ")
+        if node.left: queue.insert(0,node.left)
+        if node.right: queue.insert(0,node.right)
